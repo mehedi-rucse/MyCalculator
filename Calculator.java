c1d35c5cb0230b73e83ee2aea4ec64c896d33a82
Md. Mehedi hasan <mehedi.rucse@gmail.com>
2019-04-17 10:11:51 +0600

    Main class was added

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        double firstValue = Double.parseDouble(args[0]);
+        double secondValue = Double.parseDouble(args[1]);
+
+    }
+}
+
